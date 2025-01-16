bb056f1c5072354730b015eecb7f6f590e05b937
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-01-16 15:59:33 +0000

    deleted commented codes

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -19,6 +19,7 @@ const StartupForm = () => {
     const { toast } = useToast();
     const router = useRouter();
 
+
     const handleFormSubmit = async (prevState: any, formData: FormData) => {
         try {
             const formValues = {
